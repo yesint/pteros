b57485ba94e97bf279c1dfabc9547da8fac4b3da
yesint4 <yesint4@users.sourceforge.net>
2013-01-15 17:17:45 +0200

    Midline search in curved_membrane

--- src/examples/curved_membrane.cpp
+++ src/examples/curved_membrane.cpp
@@ -16,23 +16,20 @@ protected:
     virtual void pre_process(){
         po4.modify(system,"name PO4");
 
-        cout << "PO4: " << po4.size() << endl;
-        // Set beta for all PO4 atoms to 1 - this indicates that
-        // All of them are available
-        po4.set_beta(0);
-
-        parts.resize(2);                       
-
-        out.open("/media/data/semen/trajectories/grand_challenge/midline.dat");
+        cout << "PO4: " << po4.size() << endl;        
     }
 
     virtual bool process_frame(const Frame_info& info){
         try{
 
-        cout << "Frame " << info.absolute_frame << endl;
+        cout << "Frame " << info.valid_frame << endl;
+        out.open(string("/media/data/semen/trajectories/grand_challenge/midline_"
+                        + boost::lexical_cast<string>(info.valid_frame)+".dat").c_str());
 
         Bilayer_point_info inf;
         Vector3f Y(0,1,0);
+        Vector3f init_pos;
+        Vector3f tmp;
 
         if(info.valid_frame ==0 ){
             po4.write("/media/data/semen/trajectories/grand_challenge/sp.pdb");
@@ -40,76 +37,44 @@ protected:
 
             lipids.modify(system,"resname DOPC DOPS");
             bi.create(lipids,"name PO4",2.0);
-            inf = bi.point_info(po4.XYZ(0));
-            // Determine starting point
-            current_end = inf.center;
-            cout << "draw sphere \"" << inf.center.transpose()*10 << "\" radius 10" << endl;
-            for(int i=0;i<20;++i){
-                inf = bi.point_info(current_end);
-                current_end = inf.center + inf.normal.cross(Y).normalized()*2.0;
-                cout << "draw sphere \"" << current_end.transpose()*10 << "\" radius 10" << endl;
+        }
+
+        inf = bi.point_info(po4.XYZ(0));
+        // Determine starting point
+        current_end = inf.center;
+        //cout << "draw sphere \"" << inf.center.transpose()*10 << "\" radius 10" << endl;
+
+        int iter = 0;
+        int NY = 5;
+        float shift = 2.0;
+        float Ystep = system.Box(0)(1,1)/float(NY+1);
+        for(;;){
+            iter++;
+            // We are computing average of bilayer centers by going along Y
+            // Taking 10 points distributed over whole Y dimension
+            tmp = current_end;
+            tmp(1) = 0;
+            current_end.fill(0);
+            Vector3f aver_normal;
+            aver_normal.fill(0);
+            for(int i=0;i<NY;++i){
+                inf = bi.point_info(tmp);
+                current_end += inf.center;
+                aver_normal += inf.normal;
+                tmp(1) += Ystep;
             }
-
-            /*
-            cur.modify(system,"name PO4 and beta=0 and distance pbc point "+
-                       boost::lexical_cast<string>(current_end(0)) + " " +
-                       boost::lexical_cast<string>(current_end(1)) + " " +
-                       boost::lexical_cast<string>(current_end(2)) +
-                       " < 30"
-                      );
-            cout << "cur: " << cur.size() << endl;
-            cur.set_beta(1); // Exclude
-            current_end = cur.center(false,true);
-            cout << "Starting end:" << current_end.transpose() << endl;
+            current_end /= float(NY);
+            //cout << "draw sphere \"" << current_end.transpose()*10 << "\" radius 10" << endl;
             out << current_end(0) << " " << current_end(2) << endl;
-            */
+
+            if(iter==1) init_pos = current_end;
+
+            current_end += aver_normal.cross(Y).normalized()*shift;
+
+            if((init_pos-current_end).norm()<shift && iter>10) break;
         }
-        // Make selection around current center
-        // Include only atoms with beta>0
-
-        /*
-        cur.modify(system,"beta=0 and name PO4 and distance pbc point "+
-                   boost::lexical_cast<string>(current_end(0)) + " " +
-                   boost::lexical_cast<string>(current_end(1)) + " " +
-                   boost::lexical_cast<string>(current_end(2)) +
-                   " < 31"
-                  );
-
-        cout << "cur: " << cur.size() << endl;        
-        //cur.set_beta(2);
-
-        // Split obtained selection into two peaces in forward and backward direction
-        cur.split_by_connectivity(10,parts);
-        //parts[0].set_beta(2);
-        //parts[1].set_beta(3);
-        //po4.write("/media/data/semen/trajectories/grand_challenge/sp.pdb");
-
-        current_end = parts[0].center(false,true);
-        parts[0].set_beta(2);
-        inf = bi.point_info(current_end);
-        out << inf.center(0) << " " << inf.center(2) << endl;
-        cout << "draw sphere \"" << inf.center.transpose()*10 << "\" radius 10" << endl;
-        //Vector3f p2 = parts[1].center(false,true);
-
-        //out << p1(0) << " " << p1(2) << endl;
-        //out << p2(0) << " " << p2(2) << endl;
-
-        for(int i=0;i<10;++i){
-            cur.modify(system,"beta=0 and name PO4 and distance pbc point "+
-                       boost::lexical_cast<string>(current_end(0)) + " " +
-                       boost::lexical_cast<string>(current_end(1)) + " " +
-                       boost::lexical_cast<string>(current_end(2)) +
-                       " < 31"
-                      );
-            cur.set_beta(3+i);
-            current_end = cur.center(false,true);
-            inf = bi.point_info(current_end);
-            out << inf.center(0) << " " << inf.center(2) << endl;
-            cout << "draw sphere \"" << inf.center.transpose()*10 << "\" radius 10" << endl;
-        }
-
-        po4.write("/media/data/semen/trajectories/grand_challenge/sp.pdb");
-        */
+        cout << "Done in " << iter << " steps" << endl;
+        out.close();
 
         return true;
 
@@ -119,7 +84,6 @@ protected:
     }
 
     virtual void post_process(const Frame_info& info){
-        out.close();
 
     }
 
