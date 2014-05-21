8320e938939268094509b60b74a2645698fed00f
Semen Yesylevskyy <yesint4@yahoo.com>
2014-05-21 13:31:24 +0200

    Refactoring of overloads treatment for Periodic_box bindings

--- src/python/bindings/bindings_periodic_box.cpp
+++ src/python/bindings/bindings_periodic_box.cpp
@@ -71,92 +71,65 @@ PyObject* Periodic_box_extents(Periodic_box* b){
     return boost::python::incref(ret);
 }
 
-float Periodic_box_distance1(Periodic_box* b, PyObject* point1, PyObject* point2,
-                             bool do_wrap, PyObject* periodic_dims){
+float Periodic_box_distance(Periodic_box* b, PyObject* point1, PyObject* point2,
+                             bool do_wrap=true, PyObject* periodic_dims=nullptr){
     MAP_EIGEN_TO_PYTHON_F(Vector3f,p1,point1)
     MAP_EIGEN_TO_PYTHON_F(Vector3f,p2,point2)
-    MAP_EIGEN_TO_PYTHON_I(Vector3i,dim,periodic_dims)
-    return b->distance(p1,p2,do_wrap,dim);
+    if(periodic_dims){
+        MAP_EIGEN_TO_PYTHON_I(Vector3i,dim,periodic_dims)
+        return b->distance(p1,p2,do_wrap,dim);
+    } else {
+        return b->distance(p1,p2,do_wrap);
+    }
 }
 
-float Periodic_box_distance2(Periodic_box* b, PyObject* point1, PyObject* point2, bool do_wrap){
+BOOST_PYTHON_FUNCTION_OVERLOADS(Periodic_box_distance_overloads,Periodic_box_distance,3,5);
+
+void Periodic_box_wrap_point(Periodic_box* b, PyObject* point, PyObject* dims_to_wrap=nullptr){
+    MAP_EIGEN_TO_PYTHON_F(Vector3f,p,point)
+    if(dims_to_wrap){
+        MAP_EIGEN_TO_PYTHON_I(Vector3i,dim,dims_to_wrap)
+        b->wrap_point(p,dim);
+    } else {
+        b->wrap_point(p);
+    }
+}
+
+BOOST_PYTHON_FUNCTION_OVERLOADS(Periodic_box_wrap_point_overloads,Periodic_box_wrap_point,2,3);
+
+PyObject* Periodic_box_get_closest_image(Periodic_box* b, PyObject* point, PyObject* target,
+                             bool do_wrap=true, PyObject* dims_to_wrap=nullptr){
+    MAP_EIGEN_TO_PYTHON_F(Vector3f,p,point)
+    MAP_EIGEN_TO_PYTHON_F(Vector3f,t,target)   
+    CREATE_PYARRAY_1D_AND_MAP(ret,Vector3f,v,3)
+    if(dims_to_wrap){
+        MAP_EIGEN_TO_PYTHON_I(Vector3i,dim,dims_to_wrap)
+        v = b->get_closest_image(p,t,do_wrap,dim);
+        return boost::python::incref(ret);
+    } else {
+        v = b->get_closest_image(p,t,do_wrap);
+        return boost::python::incref(ret);
+    }
+}
+
+BOOST_PYTHON_FUNCTION_OVERLOADS(Periodic_box_get_closest_image_overloads,Periodic_box_get_closest_image,3,5);
+
+PyObject* Periodic_box_shortest_vector(Periodic_box* b, PyObject* point1, PyObject* point2,
+                                        PyObject* dims_to_wrap=nullptr){
     MAP_EIGEN_TO_PYTHON_F(Vector3f,p1,point1)
-    MAP_EIGEN_TO_PYTHON_F(Vector3f,p2,point2)
-    return b->distance(p1,p2,do_wrap);
-}
-
-float Periodic_box_distance3(Periodic_box* b, PyObject* point1, PyObject* point2){
-    MAP_EIGEN_TO_PYTHON_F(Vector3f,p1,point1)
-    MAP_EIGEN_TO_PYTHON_F(Vector3f,p2,point2)
-    return b->distance(p1,p2);
-}
-
-void Periodic_box_wrap_point1(Periodic_box* b, PyObject* point, PyObject* dims_to_wrap){
-    MAP_EIGEN_TO_PYTHON_F(Vector3f,p,point)
-    MAP_EIGEN_TO_PYTHON_I(Vector3i,dim,dims_to_wrap)
-    b->wrap_point(p,dim);
-}
-
-void Periodic_box_wrap_point2(Periodic_box* b, PyObject* point){
-    MAP_EIGEN_TO_PYTHON_F(Vector3f,p,point)
-    b->wrap_point(p);
-}
-
-PyObject* Periodic_box_get_closest_image1(Periodic_box* b, PyObject* point, PyObject* target,
-                             bool do_wrap, PyObject* dims_to_wrap){
-    MAP_EIGEN_TO_PYTHON_F(Vector3f,p,point)
-    MAP_EIGEN_TO_PYTHON_F(Vector3f,t,target)
-    MAP_EIGEN_TO_PYTHON_I(Vector3i,dim,dims_to_wrap)
-
+    MAP_EIGEN_TO_PYTHON_F(Vector3f,p2,point2)    
     CREATE_PYARRAY_1D_AND_MAP(ret,Vector3f,v,3)
-
-    v = b->get_closest_image(p,t,do_wrap,dim);
-    return boost::python::incref(ret);
+    if(dims_to_wrap){
+        MAP_EIGEN_TO_PYTHON_I(Vector3i,dim,dims_to_wrap)
+        v = b->shortest_vector(p1,p2,dim);
+        return boost::python::incref(ret);
+    } else {
+        v = b->shortest_vector(p1,p2);
+        return boost::python::incref(ret);
+    }
 }
 
-PyObject* Periodic_box_get_closest_image2(Periodic_box* b, PyObject* point, PyObject* target,
-                             bool do_wrap){
-    MAP_EIGEN_TO_PYTHON_F(Vector3f,p,point)
-    MAP_EIGEN_TO_PYTHON_F(Vector3f,t,target)
-
-    CREATE_PYARRAY_1D_AND_MAP(ret,Vector3f,v,3)
-
-    v = b->get_closest_image(p,t,do_wrap);
-    return boost::python::incref(ret);
-}
-
-PyObject* Periodic_box_get_closest_image3(Periodic_box* b, PyObject* point, PyObject* target){
-    MAP_EIGEN_TO_PYTHON_F(Vector3f,p,point)
-    MAP_EIGEN_TO_PYTHON_F(Vector3f,t,target)
-
-    CREATE_PYARRAY_1D_AND_MAP(ret,Vector3f,v,3)
-
-    v = b->get_closest_image(p,t);
-    return boost::python::incref(ret);
-}
-
-PyObject* Periodic_box_shortest_vector1(Periodic_box* b, PyObject* point1, PyObject* point2,
-                                        PyObject* dims_to_wrap){
-    MAP_EIGEN_TO_PYTHON_F(Vector3f,p1,point1)
-    MAP_EIGEN_TO_PYTHON_F(Vector3f,p2,point2)
-    MAP_EIGEN_TO_PYTHON_I(Vector3i,dim,dims_to_wrap)
-
-    CREATE_PYARRAY_1D_AND_MAP(ret,Vector3f,v,3)
-
-    v = b->shortest_vector(p1,p2,dim);
-    return boost::python::incref(ret);
-}
-
-
-PyObject* Periodic_box_shortest_vector2(Periodic_box* b, PyObject* point1, PyObject* point2){
-    MAP_EIGEN_TO_PYTHON_F(Vector3f,p1,point1)
-    MAP_EIGEN_TO_PYTHON_F(Vector3f,p2,point2)
-
-    CREATE_PYARRAY_1D_AND_MAP(ret,Vector3f,v,3)
-
-    v = b->shortest_vector(p1,p2);
-    return boost::python::incref(ret);
-}
+BOOST_PYTHON_FUNCTION_OVERLOADS(Periodic_box_shortest_vector_overloads,Periodic_box_shortest_vector,3,4);
 
 
 void make_bindings_Periodic_box(){
@@ -173,16 +146,10 @@ void make_bindings_Periodic_box(){
         .def("extents",&Periodic_box_extents)
         .def("is_triclinic",&Periodic_box::is_triclinic)
         .def("is_periodic",&Periodic_box::is_periodic)
-        .def("distance",&Periodic_box_distance1)
-        .def("distance",&Periodic_box_distance2)
-        .def("distance",&Periodic_box_distance3)
-        .def("wrap_point",&Periodic_box_wrap_point1)
-        .def("wrap_point",&Periodic_box_wrap_point2)
-        .def("get_closest_image",&Periodic_box_get_closest_image1)
-        .def("get_closest_image",&Periodic_box_get_closest_image2)
-        .def("get_closest_image",&Periodic_box_get_closest_image3)
-        .def("shortest_vector",&Periodic_box_shortest_vector1)
-        .def("shortest_vector",&Periodic_box_shortest_vector2)
+        .def("distance",&Periodic_box_distance, Periodic_box_distance_overloads())
+        .def("wrap_point",&Periodic_box_wrap_point, Periodic_box_wrap_point_overloads())
+        .def("get_closest_image",&Periodic_box_get_closest_image, Periodic_box_get_closest_image_overloads())
+        .def("shortest_vector",&Periodic_box_shortest_vector, Periodic_box_shortest_vector_overloads())
         .def("is_periodic",&Periodic_box::volume)
     ;
 }
