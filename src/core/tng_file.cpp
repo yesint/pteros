0596d1271b1648bde699e6bc1ac5e8cc5a5b7cee
Semen Yesylevskyy <yesint4@yahoo.com>
2014-08-04 15:15:47 +0300

    Initial support of tng with VMD plugin from 1.9.2alpha. Currently doesn't work :)

---
+++ src/core/tng_file.cpp
@@ -0,0 +1,36 @@
+/*
+ *
+ *                This source code is part of
+ *                    ******************
+ *                    ***   Pteros   ***
+ *                    ******************
+ *                 molecular modeling library
+ *
+ * Copyright (c) 2009-2013, Semen Yesylevskyy
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of Artistic License:
+ *
+ * Please note, that Artistic License is slightly more restrictive
+ * then GPL license in terms of distributing the modified versions
+ * of this software (they should be approved first).
+ * Read http://www.opensource.org/licenses/artistic-license-2.0.php
+ * for details. Such license fits scientific software better then
+ * GPL because it prevents the distribution of bugged derivatives.
+ *
+*/
+
+#include "tng_file.h"
+#include "molfile_plugin.h"
+
+using namespace std;
+using namespace pteros;
+using namespace Eigen;
+
+extern molfile_plugin_t tng_plugin;
+
+TNG_file::TNG_file(string fname, char open_mode): VMD_molfile_plugin_wrapper(fname,open_mode){
+    plugin = &tng_plugin;
+    accepted_format = TNG_FILE;
+    open(fname,open_mode);
+}
