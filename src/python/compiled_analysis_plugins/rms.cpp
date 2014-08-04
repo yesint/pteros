0fdeb4d68f1f11ebedbe049ba82d9f30761e2800
Semen Yesylevskyy <yesint4@yahoo.com>
2014-08-04 11:03:13 +0300

    Fixed minor formatting bug in plugins help messages

--- src/python/compiled_analysis_plugins/rms.cpp
+++ src/python/compiled_analysis_plugins/rms.cpp
@@ -44,7 +44,7 @@ public:
                 "\t-selection <string>\n"
                 "\t\tSelection text"
                 "\t-nojump <true|false>. Default: true\n"
-                "\t\tRemove jumps of atoms over periodic box boundary."
+                "\t\tRemove jumps of atoms over periodic box boundary.\n"
                 "\t\tSetting this to false is only meaningful is very special cases."
                 ;
     }
