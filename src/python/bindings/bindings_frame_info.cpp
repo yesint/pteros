43a25da15742d6607c60ff7e997a5e1aa594c039
Semen Yesylevskyy <yesint4@yahoo.com>
2016-12-10 10:43:49 +0200

    Removed pickling from Frame_info bindings

--- src/python/bindings/bindings_frame_info.cpp
+++ src/python/bindings/bindings_frame_info.cpp
@@ -26,39 +26,15 @@
 using namespace boost::python;
 using namespace pteros;
 
-struct Frame_info_suite : boost::python::pickle_suite
-{
-    static boost::python::tuple getstate(boost::python::object w_obj)
-    {
-        Frame_info const& w = boost::python::extract<Frame_info const&>(w_obj)();
-        return boost::python::make_tuple(
-                  w_obj.attr("__dict__"), //If the python object has other attributes, they will be stored in the dict
-                  w.absolute_frame,
-                  w.absolute_time,
-                  w.valid_frame,
-                  w.first_time,
-                  w.last_time,
-                  w.first_frame,
-                  w.last_frame);
-    }
-
-    static void setstate(boost::python::object w_obj, boost::python::tuple state)
-    {
-        using namespace boost::python;
-        Frame_info& w = extract<Frame_info&>(w_obj)();
-        // restore the object's __dict__
-        dict d = extract<dict>(w_obj.attr("__dict__"))();
-        d.update(state[0]);
-        w.absolute_frame = extract<int>(state[1]);
-        w.absolute_time = extract<float>(state[2]);
-        w.valid_frame = extract<int>(state[3]);
-        w.first_time = extract<float>(state[4]);
-        w.last_time = extract<float>(state[5]);
-        w.first_frame = extract<int>(state[6]);
-        w.last_frame = extract<int>(state[7]);
-    }
-    static bool getstate_manages_dict() { return true; }
-};
+void _set(Frame_info* f, boost::python::tuple data){
+    f->absolute_frame = extract<int>(data[0]);
+    f->absolute_time = extract<float>(data[1]);
+    f->valid_frame = extract<int>(data[2]);
+    f->first_time = extract<float>(data[3]);
+    f->last_time = extract<float>(data[4]);
+    f->first_frame = extract<int>(data[5]);
+    f->last_frame = extract<int>(data[6]);
+}
 
 void make_bindings_Frame_info(){
 
@@ -70,7 +46,6 @@ void make_bindings_Frame_info(){
         .def_readonly("last_frame",&Frame_info::last_frame)
         .def_readonly("last_time",&Frame_info::last_time)
         .def_readonly("valid_frame",&Frame_info::valid_frame)
-        .enable_pickling()
-        .def_pickle(Frame_info_suite())
+        .def("_set",&_set)
     ;
 }
