b8e085af881f81812cd36e54f5b440afa402edfd
Semen Yesylevskyy <yesint4@yahoo.com>
2017-08-30 20:15:25 +0300

    Updated year in copyright message

--- src/core/distance_search/distance_search_contacts.h
+++ src/core/distance_search/distance_search_contacts.h
@@ -6,7 +6,7 @@
  *                    ******************
  *                 molecular modeling library
  *
- * Copyright (c) 2009-2013, Semen Yesylevskyy
+ * Copyright (c) 2009-2017, Semen Yesylevskyy
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of Artistic License:
