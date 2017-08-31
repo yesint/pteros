4bf7e80c48eec8f4e8e9f7d70b25394129d3e89a
Semen Yesylevskyy <yesint4@yahoo.com>
2017-08-31 16:54:34 +0300

    Added main new python module

---
+++ src/python/bindings11/pteros11.py
@@ -0,0 +1,8 @@
+from _pteros11 import *
+
+
+class Task_base(Task_base_):
+    def __init__(self,opt):
+        Task_base_.__init__(self,opt)
+        # Set class name for logger initialization on C++ side
+        self._class_name = type(self).__name__
