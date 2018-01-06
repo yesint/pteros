1f4c975420e3e6076366de12b909a6753c2e0747
Semen Yesylevskyy <yesint4@yahoo.com>
2018-01-06 19:36:35 +0200

    fixes to licence and file encoding

--- include/pteros/core/selection.h
+++ include/pteros/core/selection.h
@@ -1,22 +1,26 @@
 /*
+ * This file is a part of
  *
- *                This source code is part of
- *                    ******************
- *                    ***   Pteros   ***
- *                    ******************
- *                 molecular modeling library
+ * ============================================
+ * ###   Pteros molecular modeling library  ###
+ * ============================================
  *
- * Copyright (c) 2009-2015, Semen Yesylevskyy
+ * (C) 2009-2018, Semen Yesylevskyy
  *
- * All works, which use Pteros, should cite:
+ * All works, which use Pteros, should cite the following papers:
+ *  
+ *  1.  Semen O. Yesylevskyy, "Pteros 2.0: Evolution of the fast parallel
+ *      molecular analysis library for C++ and python",
+ *      Journal of Computational Chemistry, 2015, 36(19), 1480–1488.
+ *      doi: 10.1002/jcc.23943.
  *
- *     Semen O. Yesylevskyy, "Pteros: Fast and easy to use open-source
- *     C++ library for molecular analysis",
- *     Journal of Computational Chemistry, 2012, 33(19), 1632–1636.
+ *  2.  Semen O. Yesylevskyy, "Pteros: Fast and easy to use open-source C++
+ *      library for molecular analysis",
+ *      Journal of Computational Chemistry, 2012, 33(19), 1632–1636.
+ *      doi: 10.1002/jcc.22989.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of Artistic License:
- * Read http://www.opensource.org/licenses/artistic-license-2.0.php
+ * This is free software distributed under Artistic License:
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
  *
 */
 
