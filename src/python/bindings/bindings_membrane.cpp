baf2926e2612485f0a8bbd83ba51a5063f910030
Semen Yesylevskyy <yesint4@yahoo.com>
2021-07-02 14:30:48 +0200

    Formatting fixes for LipidMembrane

--- src/python/bindings/bindings_membrane.cpp
+++ src/python/bindings/bindings_membrane.cpp
@@ -36,76 +36,6 @@ using namespace pybind11::literals;
 
 void make_bindings_Membrane(py::module& m){
 
-    py::class_<Lipid_descr>(m,"Lipid_descr")
-        .def(py::init<const std::string&,
-             const std::string&,
-             const std::string&,
-             const std::string&,
-             const std::string&,
-             const std::vector<std::string>&>())
-        .def_readonly("name",&Lipid_descr::name)
-        .def_readonly("whole_sel_str",&Lipid_descr::whole_sel_str)
-        .def_readonly("head_sel_str",&Lipid_descr::head_sel_str)
-        .def_readonly("tail_sel_str",&Lipid_descr::tail_sel_str)
-        .def_readonly("head_mid_str",&Lipid_descr::mid_sel_str)
-        .def_readonly("head_mid_str",&Lipid_descr::tail_carbon_sels)
-    ;
-
-    py::class_<Splay_pair>(m,"Splay_pair")
-        .def_readonly("lip1",&Splay_pair::lip1)
-        .def_readonly("lip2",&Splay_pair::lip2)
-        .def_readonly("splay",&Splay_pair::splay)
-    ;
-
-    py::class_<Lipid>(m,"Lipid")
-            .def(py::init<const Selection&,const Lipid_descr&>())
-
-            .def_readwrite("mid_sel",&Lipid::mid_sel)
-            .def_readwrite("head_sel",&Lipid::head_sel)
-            .def_readwrite("tail_sel",&Lipid::tail_sel)
-            .def_readwrite("whole_sel",&Lipid::whole_sel)
-            .def_readwrite("group",&Lipid::group)
-
-            .def_readonly("name",&Lipid::name)            
-            .def_readonly("normal",&Lipid::normal)
-            .def_readonly("smoothed_mid_xyz",&Lipid::smoothed_mid_xyz)
-            .def_readonly("tilt",&Lipid::tilt)            
-            .def_readonly("quad_fit_rms",&Lipid::quad_fit_rms)
-            .def_readonly("area",&Lipid::area)
-            .def_readonly("leaflet",&Lipid::leaflet)
-            .def_readonly("gaussian_curvature",&Lipid::gaussian_curvature)
-            .def_readonly("mean_curvature",&Lipid::mean_curvature)
-            .def_readonly("coord_number",&Lipid::coord_number)
-            .def_readonly("order",&Lipid::order)
-            .def_readonly("dipole",&Lipid::dipole)
-            .def_readonly("dipole_proj",&Lipid::dipole_proj)
-    ;
-
-    py::class_<Membrane>(m,"Membrane")
-        .def(py::init<System*,const std::vector<Lipid_descr>&>())
-        .def(py::init<System*,const std::vector<Lipid_descr>&,int>())
-        .def(py::init<System*,const std::vector<Lipid_descr>&,int,bool>())
-
-        .def("compute_properties",&Membrane::compute_properties,
-             "d"_a,
-             "use_external_normal"_a=false,
-             "external_pivot"_a=Eigen::Vector3f::Zero(),
-             "external_dist_dim"_a=Eigen::Vector3i::Ones()
-            )
-        .def("compute_averages",&Membrane::compute_averages)
-        .def("write_averages",&Membrane::write_averages)
-        .def("write_vmd_arrows",&Membrane::write_vmd_arrows)
-        .def("write_smoothed",&Membrane::write_smoothed)
-        .def("num_lipids",&Membrane::num_lipids)
-        .def("get_lipid",&Membrane::get_lipid,py::return_value_policy::reference_internal)        
-        .def_readonly("lipids",&Membrane::lipids)
-        .def_readonly("splay",&Membrane::splay)
-        .def_readonly("neighbors",&Membrane::neighbors)        
-    ;
-
-    //----------------------------------
-    // New classes
-    //----------------------------------
     py::class_<LipidSpecies>(m,"LipidSpecies")
         .def(py::init<const std::string&,
              const std::string&,
@@ -123,8 +53,7 @@ void make_bindings_Membrane(py::module& m){
 
 
     py::class_<LipidMolecule>(m,"LipidMolecule")
-        .def(py::init<const Selection&,const LipidSpecies&,int,LipidMembrane*>())
-
+        //.def(py::init<const Selection&,const LipidSpecies&,int,LipidMembrane*>())
         .def_readonly("name",&LipidMolecule::name)
         .def_readwrite("mid_marker_sel",&LipidMolecule::mid_marker_sel)
         .def_readwrite("head_marker_sel",&LipidMolecule::head_marker_sel)
